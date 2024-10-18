5ef12bcabe8e86a7f8dbc67a5869bc05caca32fc
aZolo77 <azolotov579@gmail.com>
2024-10-18 18:57:56 +0400

    Legacy way to dispatch actions in Redux

--- 13-redux-intro/src/features/accounts/accountSlice.js
+++ 13-redux-intro/src/features/accounts/accountSlice.js
@@ -2,12 +2,17 @@ const initialStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
+  isLoading: false,
 };
 
 export default function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
     case "account/deposit":
-      return { ...state, balance: state.balance + action.payload };
+      return {
+        ...state,
+        balance: state.balance + action.payload,
+        isLoading: false,
+      };
     case "account/withdraw":
       return { ...state, balance: state.balance - action.payload };
     case "account/requestLoan":
@@ -26,14 +31,33 @@ export default function accountReducer(state = initialStateAccount, action) {
         loanPurpose: "",
         balance: state.balance - state.loan,
       };
+    case "account/convertingCurrency":
+      return { ...state, isLoading: true };
 
     default:
       return state;
   }
 }
 
-export function deposit(amount) {
-  return { type: "account/deposit", payload: amount };
+export function deposit(amount, currency) {
+  if (currency === "USD") return { type: "account/deposit", payload: amount };
+
+  return async function (dispatch, getState) {
+    dispatch({ type: "account/convertingCurrency" });
+
+    // #1 API CALL
+    const res = await fetch(
+      `https://api.frankfurter.app/latest?base=${currency}&symbols=USD`
+    );
+    const data = await res.json();
+    // console.log(data);
+
+    const convertedAmount = (amount * data.rates[`USD`]).toFixed(2);
+    // console.log(convertedAmount);
+
+    // #2 dispatch an Action
+    dispatch({ type: "account/deposit", payload: convertedAmount });
+  };
 }
 
 export function withdraw(amount) {
