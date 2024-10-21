142b04ec45c9d677c0552a89ae3affbfe0674d56
aZolo77 <azolotov579@gmail.com>
2024-10-21 14:47:22 +0400

    Redux Toolkit

--- 13-redux-intro/src/store.js
+++ 13-redux-intro/src/store.js
@@ -1,29 +1,12 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
-import { thunk } from "redux-thunk";
-import { composeWithDevTools } from "@redux-devtools/extension";
+import { configureStore } from "@reduxjs/toolkit";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
 
-const rootReducer = combineReducers({
-  account: accountReducer,
-  customer: customerReducer,
+const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    customer: customerReducer,
+  },
 });
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
-
 export default store;
-
-// store.dispatch(deposit(500));
-// store.dispatch(withdraw(200));
-// store.dispatch(requestLoan(1000, "Rent a Car"));
-// store.dispatch(payLoan());
-
-// console.log(store.getState());
-
-// store.dispatch(createCustomer("John Smith", "1234567"));
-// store.dispatch(updateName("Alex Zollo"));
-
-// console.log(store.getState());
