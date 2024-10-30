aa81e89282d972dbe788396d1573b061c9eaba43
aZolo77 <azolotov579@gmail.com>
2024-10-30 22:09:31 +0400

    Added user state model and cart state model

--- 14-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 14-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -1,5 +1,6 @@
 // import { useState } from "react";
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import Button from '../../ui/Button';
 import { createOrder } from '../../services/apiRestaurant';
@@ -35,6 +36,7 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
+  const username = useSelector((state) => state.user.username);
   // const [withPriority, setWithPriority] = useState(false);
   const navigation = useNavigation();
 
@@ -54,7 +56,13 @@ function CreateOrder() {
       <Form method="POST">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
-          <input className="input grow" type="text" name="customer" required />
+          <input
+            className="input grow"
+            defaultValue={username}
+            type="text"
+            name="customer"
+            required
+          />
         </div>
 
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
--- 14-fast-react-pizza/src/main.jsx
+++ 14-fast-react-pizza/src/main.jsx
@@ -1,11 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
-import "./index.css";
+import store from './store.js';
+import App from './App.jsx';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
 );
