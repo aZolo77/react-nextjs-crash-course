875a42fee7548f14e7d3dba22917f85757cb05ab
aZolo77 <azolotov579@gmail.com>
2024-11-12 23:46:35 +0400

    deleting an item with react query | displaying toasts

--- 15-the-wild-oasis/src/services/supabase.js
+++ 15-the-wild-oasis/src/services/supabase.js
@@ -1,8 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://yaylybiicrirorqfcvmx.supabase.co";
+
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlheWx5YmlpY3Jpcm9ycWZjdm14Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEwNTMyMDksImV4cCI6MjA0NjYyOTIwOX0.ExZqLXe_5y1SbRE0mUS_5KBoqHHMZ-Y2dE-zOpoKOzg";
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
