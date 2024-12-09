19991942e21e79c5c9fbaf924b12171ac8811146
aZolo77 <azolotov579@gmail.com>
2024-12-09 11:02:03 +0400

    Edited tiny issues

--- 15-the-wild-oasis/src/features/cabins/CabinRow.jsx
+++ 15-the-wild-oasis/src/features/cabins/CabinRow.jsx
@@ -53,7 +53,7 @@ const Discount = styled.div`
 
 function CabinRow({ cabin }) {
   const { isDeleting, deleteCabin } = useDeleteCabin();
-  const { createCabin } = useCreateCabin();
+  const { createCabin, isCreating } = useCreateCabin();
 
   const {
     id: cabinId,
@@ -93,7 +93,11 @@ function CabinRow({ cabin }) {
             <Menus.Toggle id={cabinId} />
 
             <Menus.List id={cabinId}>
-              <Menus.Button icon={<HiSquare2Stack />} onClick={handleDuplicate}>
+              <Menus.Button
+                icon={<HiSquare2Stack />}
+                onClick={handleDuplicate}
+                disabled={isCreating}
+              >
                 Duplicate
               </Menus.Button>
 
